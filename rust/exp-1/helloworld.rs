c489a06714e80c18a0fbe26fe9427b9dd6fe60be
septicmk <septic.mk@gmail.com>
2021-11-11 10:02:20 +0800

    update rust

--- rust/exp-1/helloworld.rs
+++ rust/exp-1/helloworld.rs
@@ -1,6 +1,18 @@
 fn main(){
+    // immutable variable
     let a=12;
-    let mut b = 13;
-    b=12;
-    println!("hello world, a is {0} and b is {1} too", a, b);
+    // a = 13;
+
+    // mutable variable
+    let mut b = 12;
+    b=b+1;
+
+    // const, should be uppercase
+    const C:i32 = 12;
+
+    // shadowing
+    let a=13;
+    let a="13";
+    
+    println!("hello world, a is {0}, b is {1}, and c is {2}", a, b, C);
 }
