98d443b32e814dbf55255e3c038ab11acf840c44
septicmk <septic.mk@gmail.com>
2021-11-11 09:53:47 +0800

    update rust

--- rust/exp-1/helloworld.rs
+++ rust/exp-1/helloworld.rs
@@ -1,4 +1,6 @@
 fn main(){
     let a=12;
-    println!("hello world, a is {0} and b is {0} too", a);
+    let mut b = 13;
+    b=12;
+    println!("hello world, a is {0} and b is {1} too", a, b);
 }
