df0221a7a1a2468eead6d5d7ffd3450a374cef32
septicmk <septic.mk@gmail.com>
2021-11-18 09:46:37 +0800

    while

--- rust/exp-3/cond_loop.rs
+++ rust/exp-3/cond_loop.rs
@@ -1,10 +1,25 @@
-fn main(){
-    let a=12;
-    let mut b=1;
-    if a > 0 { //no brackets
-        b -=1;
+fn test_cond() {
+    let a = 12;
+    let mut b = 1;
+    if a > 0 {
+        //no brackets
+        b -= 1;
     } else {
-        b +=1;
+        b += 1;
     }
     println!("b is {}", b);
+}
+
+fn test_loop() {
+    let mut num = 1;
+    while num != 4 {
+        println!("{}", num);
+        num += 1;
+    }
+    println!("loop over");
+}
+
+fn main() {
+    test_cond();
+    test_loop();
 }
